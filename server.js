819481f88374548790aad11af368af6134bc5ef1
BUNNY <panchanihardik37@gmail.com>
2024-05-26 10:27:53 +0530

    add the file

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 require("dotenv").config()
 
 app.get('/api/get',(req,res) => {
-    res.send({message:"hello back to nodejs"})
+    res.send({message:"hello Hardik Welcome to nodejs project "})
 })
 
 app.get('/api/get_user_details',(req,res) => {
     res.send({
         user:{
-            name:"satyam",
-            age:22,
+            name:"hardik",
+            lstname: "bunny",
             contact:12254
         },env:process.env.NAME
     })
